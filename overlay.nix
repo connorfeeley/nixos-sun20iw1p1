33c75a73c0cb139b7e50086307b77a71a38e58cd
Connor Feeley <git@cfeeley.org>
2023-06-02 15:05:04 -0400

    gnu-efi: 3.0.17 build is broken

--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,16 @@
 final: prev:
 
 rec {
+  # gnu-efi 3.0.17 is broken for riscv but was fixed a few patches later
+  gnu-efi = prev.gnu-efi.overrideAttrs (o: {
+    version = "3.0.18-pre";
+    src = prev.fetchFromGitHub {
+      owner = "vathpela";
+      repo = o.pname;
+      rev = "039ca9d93c1eb5601f5b1a8bd812fd4b7d4de370";
+      hash = "sha256-QWTKYePhRUFOaPT+AQCMjrV6MNtTOqyayx8+CQdM1J4=";
+    };
+  });
   sun20i-d1-spl = prev.callPackage ./spl.nix { };
   ubootLicheeRV = prev.callPackage ./uboot.nix { };
   linux_nezha = prev.callPackage ./linux.nix { };
